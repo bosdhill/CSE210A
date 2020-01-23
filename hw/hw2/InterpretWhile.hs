1adf5fd8683aba11a017cd513b508e9bce93215c
bdhillon <bdhillon1994@gmail.com>
2020-01-23 05:29:07 -0800

    need to add state...

--- hw/hw2/InterpretWhile.hs
+++ hw/hw2/InterpretWhile.hs
@@ -1,70 +1,87 @@
 module Main where
 import ParseWhile
 import WhileTypes
-import qualified Data.HashTable.IO as H
+import qualified Data.Map as M
 -- https://stackoverflow.com/questions/22624924/how-do-i-print-the-name-and-value-of-a-custom-data-type-in-haskell
 -- https://hackage.haskell.org/package/hashtables-1.2.1.1/docs/Data-HashTable-IO.html
+-- https://stackoverflow.com/questions/27660791/insert-value-into-a-map-and-then-return-it
 -- What are the return types of a while program?
 -- Maybe error, IO(), etc.
 
-eval_abinop :: ABinOp -> AExpr -> AExpr -> Integer
-eval_abinop op a1 a2 =
+eval_abinop :: M.Map String Integer -> ABinOp -> AExpr -> AExpr -> Integer
+eval_abinop state op a1 a2 =
     case op of
-        Add      -> (eval_aexpr a1) + (eval_aexpr a2)
-        Subtract -> (eval_aexpr a1) - (eval_aexpr a2)
-        Multiply -> (eval_aexpr a1) * (eval_aexpr a2)
+        Add      -> (eval_aexpr state a1) + (eval_aexpr state a2)
+        Subtract -> (eval_aexpr state a1) - (eval_aexpr state a2)
+        Multiply -> (eval_aexpr state a1) * (eval_aexpr state a2)
         -- Divide   -> (eval_aexpr a1) / (eval_aexpr a2)
 
-eval_aexpr :: AExpr -> Integer
-eval_aexpr exp =
+lookupState :: M.Map String Integer -> String -> Integer
+lookupState state k =
+    case M.lookup k state of
+        Nothing  -> 0
+        Just val -> val
+
+eval_aexpr :: M.Map String Integer -> AExpr -> Integer
+eval_aexpr state exp =
     case exp of
-        Var s            -> undefined
+        Var s            -> lookupState state s
         IntConst c       -> c
-        Neg aExp         -> eval_aexpr aExp
-        ABinary op a1 a2 -> eval_abinop op a1 a2
+        Neg aExp         -> eval_aexpr state aExp
+        ABinary op a1 a2 -> eval_abinop state op a1 a2
 
-eval_rbinary :: RBinOp -> AExpr -> AExpr -> Bool
-eval_rbinary op a1 a2 =
+eval_rbinary :: M.Map String Integer -> RBinOp -> AExpr -> AExpr -> Bool
+eval_rbinary state op a1 a2 =
     case op of
-        Greater -> (eval_aexpr a1) > (eval_aexpr a2)
-        Less    -> (eval_aexpr a1) < (eval_aexpr a2)
+        Greater -> (eval_aexpr state a1) > (eval_aexpr state a2)
+        Less    -> (eval_aexpr state a1) < (eval_aexpr state a2)
 
-eval_bbinary :: BBinOp -> BExpr -> BExpr -> Bool
-eval_bbinary op b1 b2 =
+eval_bbinary :: M.Map String Integer -> BBinOp -> BExpr -> BExpr -> Bool
+eval_bbinary state op b1 b2 =
     case op of
-        And -> (eval_bexpr b1) && (eval_bexpr b2)
-        Or  -> (eval_bexpr b1) || (eval_bexpr b2)
+        And -> (eval_bexpr state b1) && (eval_bexpr state b2)
+        Or  -> (eval_bexpr state b1) || (eval_bexpr state b2)
 
-eval_bexpr :: BExpr -> Bool
-eval_bexpr a =
+eval_bexpr :: M.Map String Integer -> BExpr -> Bool
+eval_bexpr state a =
     case a of
         BoolConst t   -> t
-        Not t         -> not (eval_bexpr t)
-        BBinary a b c -> eval_bbinary a b c
-        RBinary a b c -> eval_rbinary a b c
+        Not t         -> not (eval_bexpr state t)
+        BBinary a b c -> eval_bbinary state a b c
+        RBinary a b c -> eval_rbinary state a b c
 
-eval_while :: BExpr -> Stmt -> IO()
-eval_while a b
-    | eval_bexpr a = eval b
-    | otherwise    = undefined
+eval_while :: M.Map String Integer -> BExpr -> Stmt -> M.Map String Integer
+eval_while state a b
+    | eval_bexpr state a = eval state b
+    | otherwise          = undefined
 
--- eval_assign :: String -> AExpr
--- eval_assign var a = eval_expr a
+eval_assign :: M.Map String Integer -> String -> AExpr -> M.Map String Integer
+eval_assign state k v = M.insert k (eval_aexpr state v) state
+                        -- do
+                        --     let v' = (eval_aexpr state v) in
+                        --         let newState = M.insert k v' state in
+                        --             return newState
 
-eval_if :: BExpr -> Stmt -> Stmt -> IO()
-eval_if bexpr s1 s2
-    | eval_bexpr bexpr = eval s1
-    | otherwise        = eval s2
+eval_if :: M.Map String Integer -> BExpr -> Stmt -> Stmt -> M.Map String Integer
+eval_if state bexpr s1 s2
+    | eval_bexpr state bexpr = eval state s1
+    | otherwise              = eval state s2
 
-eval :: Stmt -> IO()
-eval s =
+eval :: M.Map String Integer -> Stmt -> M.Map String Integer
+eval state s =
     case s of
-        Assign a b -> undefined
-        If a b c   -> undefined
-        While a b  -> eval_while a b
-        Skip       -> return () -- how to evaluate the next statement?
-        _          -> undefined
+        Seq []     -> undefined -- print all values in hashtable
+        Seq x      -> do
+                        let newState = eval state $ head x in
+                            eval newState $ Seq $ tail x
+        Assign a b -> eval_assign state a b
+        If a b c   -> eval_if state a b c
+        While a b  -> eval_while state a b
+        Skip       -> return state
 
 main :: IO ()
-main =
-    eval (parseString ("x := 1; x := 1; while x < 3 do skip"))
+main = do
+    let state = M.fromList [] in
+        let m = eval state (parseString ("while true do skip")) in
+            let f result k a = result ++ "(" ++ (show k) ++ ":" ++ a in
+                putStrLn $ M.foldlWithKey f "Map: " m
