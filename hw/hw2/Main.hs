0a277dddb70b26727f0145486fdfcf5425a18da7
bdhillon <bdhillon1994@gmail.com>
2020-01-23 21:21:18 -0800

    cleaned up main

--- hw/hw2/Main.hs
+++ hw/hw2/Main.hs
@@ -7,11 +7,10 @@ import qualified Data.Map as M
 main :: IO ()
 main = do
     stmt' <- getContents
-    let stmt = filter (\x -> not (any (x ==) "\n\r\t")) stmt' in
-        let state = M.fromList [] in
-            let m = eval state (parseString stmt) in
-                let f result k a =
-                        case result of
-                            "{"       -> result ++ k ++ " → " ++ (show a)
-                            otherwise -> result ++ ", " ++ k ++ " → " ++ (show a) in
-                    putStrLn ((M.foldlWithKey f "{" m) ++ "}")
+    let stmt = filter (\x -> not (any (x ==) "\n\r\t")) stmt'
+    let state = M.fromList []
+    let m = eval state (parseString stmt)
+    let f result k a = case result of
+            "{"       -> result ++ k ++ " → " ++ (show a)
+            otherwise -> result ++ ", " ++ k ++ " → " ++ (show a) in
+            putStrLn ((M.foldlWithKey f "{" m) ++ "}")
